617b65ecc15d23f44412eacfbd023f44dca902a2
rnarang <r.narang0102@gmail.com>
2025-09-29 21:08:17 +0530

    New commit in NumberOfDigits

--- src/test/java/NumberOfDigits.java
+++ src/test/java/NumberOfDigits.java
@@ -5,11 +5,22 @@ public class NumberOfDigits {
        NumberOfDigits numberOfDigits = new NumberOfDigits();
        numberOfDigits.numberOfDigits();
     }
-    void numberOfDigits()
-    {
-        int number = 787989;
-
-
+    void numberOfDigits() {
+        int number = 690;
+        int count = 0;
+        if(number<0) {
+            number = number*-1;
+        }
+        if(number==0)
+        {
+            count =1;
+        }
+        while (number > 0) {
+            number = number / 10;
+            count++;
+        }
+        System.out.println(count);
 
     }
+
 }
